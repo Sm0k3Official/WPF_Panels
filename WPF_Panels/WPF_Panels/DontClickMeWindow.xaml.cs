fafdcac4543522b07fbb7ef46d2f66412555a52e
DobreCosmin <cosmyn_dobre@yahoo.com>
2023-02-23 22:12:10 +0200

    Resolved a bug for main window. Added 2 more windows (don't click me window & login window). Added events for opening the windows.

---
+++ WPF_Panels/WPF_Panels/DontClickMeWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Panels
+{
+    /// <summary>
+    /// Interaction logic for DontClickMeWindow.xaml
+    /// </summary>
+    public partial class DontClickMeWindow : Window
+    {
+        public bool IsClosed { private set; get; }
+        public DontClickMeWindow()
+        {
+            InitializeComponent();
+            IsClosed = false;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            IsClosed = true;
+        }
+    }
+}
--- WPF_Panels/WPF_Panels/MainWindow.xaml.cs
+++ WPF_Panels/WPF_Panels/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace WPF_Panels
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool areEventsChecked { set; get; }
+
+        private DontClickMeWindow dontClickMeWindow;
+        private LoginWindow loginWindow;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            areEventsChecked = false;
+        }
+
+        //Don't click me methods
+        private void Button_DontClickMe(object sender, RoutedEventArgs e)
+        {
+            dontClickMeWindow = new DontClickMeWindow();
+
+            if(areEventsChecked)
+            {
+                dontClickMeWindow.Closed += DontClickMeWindow_Closed;
+                dontClickMeWindow.Show();
+
+                dontClickMeButton.IsEnabled = false;
+            }
+            else 
+            {
+                dontClickMeWindow.ShowDialog();
+            }
+        }
+
+        private void DontClickMeWindow_Closed(object sender, EventArgs e)
+        {
+            dontClickMeButton.IsEnabled = true;
+        }
+        
+        //Checkbox for activating or deactivating the events
+        private void CheckBox_Events(object sender, RoutedEventArgs e)
+        {
+            areEventsChecked = true;
+        }
+
+        private void UncheckBox_Events(object sender, RoutedEventArgs e)
+        {
+            areEventsChecked = false;
+        }
+
+        //Login methods
+        private void Button_Login(object sender, RoutedEventArgs e)
+        {
+            loginWindow = new LoginWindow();
+
+            // to be continued
         }
     }
 }
